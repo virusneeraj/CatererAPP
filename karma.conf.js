e8e8ac241a7cc6aaad1b857d88fd2c2f82c9621d
Neeraj <virusneerajkumarjha@gmail.com>
2022-06-25 20:34:04 +0530

    add server

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-my-app'),
+      dir: require('path').join(__dirname, './coverage/caterer-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+import {SortOrder} from "../app/components/caterer/model/pagination/sort-order.enum";
+
 export const environment = {
-  catererion: true
+  production: true,
+  api_success_status: ['API_200','API_201'],
+  default_settings: {
+    page: 0,
+    size: 10,
+    sort: 'id',
+    order: SortOrder.desc
+  },
+  api_url : (page: number, size: number, sort: string, order: string) => `https://virus-caterer.herokuapp.com/search?page=${page}&size=${size}&sort=${sort}:${order}`,
+  api_caterer_detail: (idOrName: string) => `https://virus-caterer.herokuapp.com/get/${idOrName}`,
+  api_caterer_create:  `https://virus-caterer.herokuapp.com/add`
 };
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in catererion mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
